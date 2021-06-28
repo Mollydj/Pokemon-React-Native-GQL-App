f7e1ac9851dc3af1aa0db6428ebb460363a67dff
Molly <75952444+MollyAtReplika@users.noreply.github.com>
2021-06-27 23:30:22 -0400

    Added Stack Navigator

--- App.js
+++ App.js
@@ -1,29 +1,54 @@
 import React, { useState } from 'react';
-import { Button, Text, View, SafeAreaView, ActivityIndicator, StyleSheet } from 'react-native';
+import {
+  Button,
+  Text,
+  View,
+  SafeAreaView,
+  ActivityIndicator,
+  StyleSheet,
+} from 'react-native';
 import { ApolloProvider, useQuery, gql } from '@apollo/client';
 import { Picker } from '@react-native-picker/picker';
 import { apolloClient } from './apollo';
 import { GET_POKEMON_BY_NAME } from './GraphQl/queries';
-import styled from 'styled-components/native'
+import styled from 'styled-components/native';
 import {
   heightPercentageToDP as hp,
   widthPercentageToDP as wp,
 } from 'react-native-responsive-screen';
 
 function RootComponent() {
-  const { data, error, loading } = useQuery(GET_POKEMON_BY_NAME, {
-    variables: { limit: 2, offset: 1 },
+  const [pokemonData, setPokemonData] = useState();
+  const { data, error, loading, refetch } = useQuery(GET_POKEMON_BY_NAME, {
+    variables: { limit: 10, offset: 1 },
+    onCompleted: (data) => {
+      setPokemonData(data);
+    },
   });
+
+  if (loading) {
+    return;
+    <Container>
+      <ActivityIndicator size='large' color='#ff0000' />
+    </Container>;
+  }
+
+  if (error) {
+    refetch();
+  }
 
   return (
     <Container>
-        {data.pokemons.results.map(item => <Pokemon>{item.name}</Pokemon>)}
+      <SafeAreaView />
+      {data.pokemons.results.map((item) => (
+        <Pokemon>{item.name}</Pokemon>
+      ))}
     </Container>
   );
 }
 
 const Container = styled.View`
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   flex: 1;
 `;
@@ -33,8 +58,9 @@ const Pokemon = styled.Text`
   width: 100%;
   justify-content: center;
   align-items: center;
-  margin: 20px;
+  margin: ${hp('1%')}px;
   font-size: ${hp('2%')}px;
+  padding: ${hp('2%')}px;
 `;
 
 export default function App() {
--- GraphQl/queries.js
+++ GraphQl/queries.js
@@ -1,20 +1,20 @@
 import { gql } from '@apollo/client';
 
 export const GET_POKEMON_BY_NAME = gql`
-query pokemons($limit: Int, $offset: Int) {
-  pokemons(limit: $limit, offset: $offset) {
-    count
-    next
-    previous
-    nextOffset
-    prevOffset
-    status
-    message
-    results {
-      url
-      name
-      image
+  query pokemons($limit: Int, $offset: Int) {
+    pokemons(limit: $limit, offset: $offset) {
+      count
+      next
+      previous
+      nextOffset
+      prevOffset
+      status
+      message
+      results {
+        url
+        name
+        image
+      }
     }
   }
-}
 `;
